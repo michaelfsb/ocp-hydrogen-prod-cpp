67ccaaa8cfbb3886f50297fc4345b4c2fa9e4a3f
Michael Barbosa <michaelfeliphe@gmail.com>
2023-07-07 20:37:49 -0300

    Add project files.

---
+++ OcpHydrogenProduction.cpp
@@ -0,0 +1,12 @@
+﻿// OcpHydrogenProduction.cpp : Defines the entry point for the application.
+//
+
+#include "OcpHydrogenProduction.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ OcpHydrogenProduction.h
@@ -0,0 +1,8 @@
+﻿// OcpHydrogenProduction.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
