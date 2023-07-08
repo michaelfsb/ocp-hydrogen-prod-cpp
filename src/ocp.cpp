dc4e33d1c3fb0e9441bac9a7c14ccdefb31f39b3
Michael Barbosa <michaelfeliphe@gmail.com>
2023-07-08 12:23:18 -0300

    Model photovoltaic panel

--- src/ocp.cpp
+++ src/ocp.cpp
@@ -1,65 +1,47 @@
 #include <iostream>
 #include <casadi/casadi.hpp>
 
+using namespace casadi;
 
-// dx/dt = f(x,u)
-casadi::MX f(const casadi::MX& x, const casadi::MX& u) {
-  return vertcat(x(1), u-x(1));
+double lambertw(const double& x) {
+    const double E = 0.4586887;
+	return (1 + E) * log(6. / 5 * x / log(12. / 5 * x / log(1 + 12. / 5 * x))) - E * log(2 * x / log(1 + 2 * x));
 }
 
+double pv_model(const double& irradiation) {
+    // Declare constants
+    const double Q = 1.6e-19; // Elementary charge
+    const double K = 1.38e-23; // Boltzmann constant
+
+    // Declare photovoltaic parameters
+    const double N_ps = 8;           // Number of panels in parallel
+    const double N_ss = 300;         // Number of panels in series
+    const double T_ps = 298;         // Temperature
+    const double Tr = 298;           // Reference temperature
+    const double Isc = 3.27;         // Short circuit current at Tr
+    const double Kl = 0.0017;        // Short circuit current temperature coeff
+    const double Ior = 2.0793e-6;    // Ior - Irs at Tr
+    const double Ego = 1.1;          // Band gap energy of the semiconductor
+    const double A = 1.6;            // Factor.cell deviation from de ideal pn junction
+
+    // Intermediate photovoltaic variables
+    const double Vt = K * T_ps / Q;
+    const double Irs = Ior * pow((T_ps / Tr),3) * exp(Q * Ego * (1 / Tr - 1 / T_ps) / (K * A));
+    double Iph = (Isc + Kl * (T_ps - Tr)) * irradiation;
+
+
+    // Algebraic equations
+    double v_ps = (N_ss * Vt * A * (lambertw(exp(1) * (Iph / Irs + 1)) - 1));
+    double i_ps = N_ps * (Iph - Irs * (exp(v_ps / (N_ss * Vt * A)) - 1));
+
+   return i_ps;
+}
+    
+
+
+
 int main(){
-  // Car race along a track
-  // ----------------------
-  // An optimal control problem (OCP),
-  // solved with direct multiple-shooting.
-  //
-  // For more information see: http://labs.casadi.org/OCP
+    double test = pv_model(1);
 
-  int N = 100; // number of control intervals
-
-  casadi::Opti opti = casadi::Opti(); // Optimization problem
-
-  casadi::Slice all;
-  // ---- decision variables ---------
-  casadi::MX X = opti.variable(2, N + 1); // state trajectory
-  auto pos = X(0, all);
-  auto speed = X(1, all);
-  casadi::MX U = opti.variable(1, N); // control trajectory (throttle)
-  casadi::MX T = opti.variable(); // final time
-
-  // ---- objective          ---------
-  opti.minimize(T); // race in minimal time
-
-  // ---- dynamic constraints --------
-  casadi::MX dt = T / N;
-  for (int k = 0; k < N; ++k) {
-    casadi::MX k1 = f(X(all,k),         U(all,k));
-    casadi::MX k2 = f(X(all,k)+dt/2*k1, U(all,k));
-    casadi::MX k3 = f(X(all,k)+dt/2*k2, U(all,k));
-    casadi::MX k4 = f(X(all,k)+dt*k3,   U(all,k));
-    casadi::MX x_next = X(all,k) + dt/6*(k1+2*k2+2*k3+k4);
-    opti.subject_to(X(all,k+1)==x_next); // close the gaps 
-  }
-
-  // ---- path constraints -----------
-  opti.subject_to(speed<=1-sin(2*casadi::pi*pos)/2); // track speed limit
-  opti.subject_to(0<=U<=1);           // control is limited
-
-  // ---- boundary conditions --------
-  opti.subject_to(pos(0)==0);   // start at position 0 ...
-  opti.subject_to(speed(0)==0); // ... from stand-still 
-  opti.subject_to(pos(N)==1); // finish line at position 1
-
-  // ---- misc. constraints  ----------
-  opti.subject_to(T>=0); // Time must be positive
-
-  // ---- initial values for solver ---
-  opti.set_initial(speed, 1);
-  opti.set_initial(T, 1);
-
-  // ---- solve NLP              ------
-  opti.solver("ipopt"); // set numerical backend
-  casadi::OptiSol sol = opti.solve();   // actual solve
-
-  return 0;
+	return 0;
 }
